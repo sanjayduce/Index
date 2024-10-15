5afd7af0052a710fb5f7e2972f092068977f92ac
sanjayduce <sandysanjay004@gmail.com>
2024-10-15 11:46:12 +0530

    Update Food.js

--- Food delivery/Food.js
+++ Food delivery/Food.js
@@ -13,7 +13,7 @@ const restaurants = {
         name: "Barbequeen",
         image: "sushi.jpg",
         menu: [
-            { name: "California Roll", price: 8, image: "Food delivery/california1.jpg" },
+            { name: "California Roll", price: 8, image: "california1.jpg" },
             { name: "Sashimi", price: 15, image: "Sashmi.jpg" },
             { name: "Miso Soup", price: 5, image: "Miso.jpg" }
         ]
